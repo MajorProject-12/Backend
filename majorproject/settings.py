7dc899d896d94d62566b60ed70e76e7f1f4656fb
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2025-04-21 20:35:53 +0530

    CSRF update

--- majorproject/settings.py
+++ majorproject/settings.py
@@ -166,8 +166,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
+
 
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
